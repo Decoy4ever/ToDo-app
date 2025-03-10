1cd453661762b02e6df5fc9c7b5c4bdc6af60d8b
David Koy <dzilla398@gmail.com>
2025-03-10 14:42:19 +1030

    Added classnames to the button links in the navbar in html file.
    Created a `class SwitchTabs`
     that prupose is to open different button links in home.js file.

--- src/home.js
+++ src/home.js
@@ -1,3 +1,44 @@
 import "./styles.css"
-import {projects} from "./project.js"
-console.log(projects)
+// import {projects} from "./project.js"
+
+// what are the share charcteristics of each list
+// 1. have a header for each link
+// 2. have add task link
+class SwitchTabs 
+{
+    constructor()
+    {
+        this.buttonTabs = document.querySelectorAll("button")
+    }
+
+    openLinkTabs()
+    {
+        const todayList = document.getElementsByClassName("today-list")
+        // loop through the buttons
+        // if the button text is equivalent to the certain text
+        // print out the a msg 
+        this.buttonTabs.forEach((btn) => {
+            btn.addEventListener("click", ()=> {
+               if(btn.textContent === "Today")
+               {
+                console.log("I click on Today list")
+               }
+               else if(btn.textContent === "List")
+               {
+                console.log("I clicked on General List")
+               }
+               else if(btn.textContent === "Completed")
+               {
+                console.log("I clicked on Completed List")
+               }
+               else if(btn.textContent === "Trash")
+               {
+                console.log("I clicked on Trash List")
+               }
+            })
+        })  
+    }
+}
+
+const tabs = new SwitchTabs()
+tabs.openLinkTabs()
