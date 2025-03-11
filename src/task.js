c319abc1941d1d1e6b28d5ad808776fb30f82a74
David Koy <dzilla398@gmail.com>
2025-03-11 14:14:26 +1030

    Modified the `addPropertiesToTask()` to create a task object that contains the properties. Task object passed into an array enabling multiple task object to be created.
    Created a `class PrintAllTasks` that prints out the array of task objects.

--- src/task.js
+++ src/task.js
@@ -9,6 +9,9 @@ class Task
     }
 }
 
+/**
+ * Create a task with the properties of title, description, duedate and priority
+ */
 class createTask extends Task
 {
     constructor(title,description,dueDate,priority)
@@ -23,26 +26,42 @@ class createTask extends Task
 
     addPropertiesToTask()
     {
-        this.taskArr.push(this.title, this.description, this.dueDate, this.priority)
-        console.log(`Successfully created Task ${this.taskArr}`)
-    }
-
-    // Display the Task Object/Element
-    showTaskDetails()
-    {
-        console.log(`Title: ${this.title}`)
-        console.log(`Description: ${this.description}`)
-        console.log(`Due Date: ${this.dueDate}`)
-        console.log(`Priority: ${this.priority}`)
-        this.addPropertiesToTask()
-        console.log(this.taskArr)
+        let task = 
+        {
+            title: this.title,
+            description: this.description,
+            dueDate: this.dueDate,
+            priority: this.priority
+        }
+        this.taskArr.push(task)
+        console.log("Successfully added task")
     }
 }
 
-export const task = new createTask()
-task.title = "Shopping"
-task.description = "Get the following fruits Apple,Banana & Orange"
-task.dueDate = "1/3/25"
-task.priority = 1
+/**
+ * Print out all the tasks
+ */
+class PrintAllTasks 
+{
+    constructor()
+    {
+        this.newTask = new createTask()
+    }
+
+    printTasksDetails()
+    {
+        this.newTask.title = prompt("Enter a title")
+        this.newTask.description = prompt("Enter a description")
+        this.newTask.dueDate = prompt("Enter a dueDate")
+        this.newTask.priority = prompt("Enter a priority")
+        this.newTask.addPropertiesToTask()
+        console.log(this.newTask.taskArr)
+    }
+}
+
+export const task = new PrintAllTasks()
+
+
+
 
 
