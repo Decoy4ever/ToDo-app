f70760510e0293ac3d1fff13dcc77fa2e186b691
David Koy <dzilla398@gmail.com>
2025-03-17 14:20:10 +1030

    Removed `addTaskObjectToArr()` in `class Task`. In addition, modified the constructor properties of `class TaskUI` and removed the `DialogUI` into the `project.js` (see previous comit).

--- src/task.js
+++ src/task.js
@@ -10,11 +10,10 @@ class Task
         this.description = description
         this.dueDate = dueDate
         this.priority = priority
-        this.taskArr = []
     }
 
-    // create a object with propertiers task
-    createTaskObject()
+    // get Tak Details
+    getTaskDetails()
     {
         return {
             Title: this.title,
@@ -23,32 +22,32 @@ class Task
             Priority: this.priority
         }
     }
-
-    // add object to taskArr
-    addTaskObjectToArr()
-    {
-        let taskObj = this.createTaskObject()
-        console.log(taskObj)
-        this.taskArr.push(taskObj)
-        return this.taskArr
-    }
 }
 
-export const task = new Task("Shopping Center","Milk & Ceral","20-3-25","High")
-
+export const task1 = new Task("Shopping Cart","Milk & Ceral","25-3-25","High")
+export const task2 = new Task("PCParts","Buy GPU", "24-5-25","Low")
 class TaskUI
-{   
-    // create the add task bar
-    addTaskBarUI()
+{  
+    constructor()
     {
+        this.tasks = new Task()
         this.mainContent = document.querySelector(".main-content")
-        const div = document.createElement("div")
-        div.classList.add("add-container")
-        
         this.form = document.createElement("form")
         this.showBtn = document.createElement("button")
         this.input = document.createElement("input")
 
+        this.title = document.querySelector(".title")
+        this.description = document.querySelector(".description")
+        this.dueDate = document.querySelector(".due-date")
+        this.priority = document.querySelector(".priority")
+    } 
+
+    // create the add task bar
+    addTaskBarUI()
+    {  
+        const div = document.createElement("div")
+        div.classList.add("add-container")
+        
         this.showBtn.textContent = "+"
         this.showBtn.setAttribute("type", "button")
         this.showBtn.setAttribute("class","addBtn")
@@ -62,40 +61,32 @@ class TaskUI
         this.mainContent.appendChild(div)
     }    
 
-    DialogUI()
-    {
+    // create the form for the dialog
+    getTaskInfo()
+    { 
+        this.contentDiv = document.createElement("div")
+        this.mainContent.appendChild(this.contentDiv)
 
-        this.dialog = document.querySelector(".dialog")
-        this.submitBtn = document.querySelector(".submit-btn");
-        this.closeModal = document.querySelector('.exit-btn');
+        let title = `${this.title.value}`
+        let description = `${this.description.value}`
+        let dueDate = `${this.dueDate.value}`
+        let priority = `${this.priority.value}`
+
+        // // create a taskObject and pass it into the 
+        const newTask = new Task(title,description,dueDate,priority)
+        return newTask
 
 
-        // show the modal
-        this.showBtn.addEventListener("click",() => {
-            this.dialog.showModal()
-        })
-
-        this.submitBtn.addEventListener('click',(e) => 
-        {
-            this.mainCard.textContent = "";    
-            console.log("form submitted")
-            e.preventDefault();
-            this.dialog.close();  
-        }) 
-
-        // close the dialog form
-        this.closeModal.addEventListener('click',(e) => 
-        {
-            e.preventDefault();
-            this.dialog.close();
-            console.log("close form activated")
-        }) 
+        // this.tasks.taskArr.forEach((task,index) => {
+        //     const divTask = document.createElement("div")
+        //     divTask.textContent = `${divTask.Title} ${divTask.Description} ${divTask.DueDate} ${divTask.Priority}`
+        //     console.log(divTask)
+        //     this.contentDiv.appendChild(divTask)
+        // })
     }
 }
 
 export const taskUI = new TaskUI()
-
-
 
 
 
